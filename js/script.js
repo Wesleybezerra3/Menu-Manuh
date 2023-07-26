21eee80452ca4ede7f6a2bf78709ef91d17d1d6d
Wesleybezerra3 <wesleybezerra770@gmail.com>
2023-07-26 11:32:45 -0300

    navbar feita (Começando mais um projeto)

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function enviarMensagem(numero) {
+    // Substitua a mensagem abaixo pela mensagem que você deseja enviar
+    var mensagem = "Olá! Estou enviando uma mensagem usando JavaScript.";
+
+    // Codifica a mensagem para que ela possa ser passada como parte do URL
+    mensagem = encodeURIComponent(mensagem);
+
+    // Cria o link com o número de telefone e a mensagem
+    var link = "https://wa.me/" + numero + "?text=" + mensagem;
+
+    // Abre o link em uma nova janela ou guia
+    window.open(link);
+  }
+  const btn = document.querySelector('button').addEventListener('click' , ()=>{
+     const numero = '5581985973850';
+     enviarMensagem(numero);
+  })
