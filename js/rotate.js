bfb6cb13d86bb224a1d0bc5fa276acff6d6e5b7f
Wesleybezerra3 <wesleybezerra770@gmail.com>
2023-07-27 14:20:59 -0300

    Navbar reposicionado

--- js/rotate.js
+++ js/rotate.js
@@ -1,10 +1,90 @@
 $(document).ready(function () {
+  let rotate = 0;
+  $('#summaryKits').click(()=>{
+      rotate += 180;
+      $('#caret-down-kits').css('transform',`rotate(${rotate}deg)`)
+      if(rotate === 180){
+        rotate =- 180;
+      }
+  })
+});
+$(document).ready(function () {
     let rotate = 0;
     $('#summaryCake').click(()=>{
         rotate += 180;
-        $('.fa-caret-down').css('transform',`rotate(${rotate}deg)`)
+        $('#caret-down-cakes').css('transform',`rotate(${rotate}deg)`)
         if(rotate === 180){
           rotate =- 180;
         }
     })
+});
+$(document).ready(function () {
+  let rotate = 0;
+  $('#summaryBrownies').click(()=>{
+      rotate += 180;
+      $('#caret-down-brownies').css('transform',`rotate(${rotate}deg)`)
+      if(rotate === 180){
+        rotate =- 180;
+      }
+  })
+});
+$(document).ready(function () {
+  let rotate = 0;
+  $('#summaryBrigadeiros').click(()=>{
+      rotate += 180;
+      $('#caret-down-brigadeiros').css('transform',`rotate(${rotate}deg)`)
+      if(rotate === 180){
+        rotate =- 180;
+      }
+  })
+});
+$(document).ready(function () {
+  let rotate = 0;
+  $('#summaryCapcukes').click(()=>{
+      rotate += 180;
+      $('#caret-down-capcakes').css('transform',`rotate(${rotate}deg)`)
+      if(rotate === 180){
+        rotate =- 180;
+      }
+  })
+});
+$(document).ready(function () {
+  let rotate = 0;
+  $('#summaryDoces').click(()=>{
+      rotate += 180;
+      $('#caret-down-doces').css('transform',`rotate(${rotate}deg)`)
+      if(rotate === 180){
+        rotate =- 180;
+      }
+  })
+});
+$(document).ready(function () {
+  let rotate = 0;
+  $('#summaryTortas').click(()=>{
+      rotate += 180;
+      $('#caret-down-tortas').css('transform',`rotate(${rotate}deg)`)
+      if(rotate === 180){
+        rotate =- 180;
+      }
+  })
+});
+$(document).ready(function () {
+  let rotate = 0;
+  $('#summaryTrunfas').click(()=>{
+      rotate += 180;
+      $('#caret-down-trunfas').css('transform',`rotate(${rotate}deg)`)
+      if(rotate === 180){
+        rotate =- 180;
+      }
+  })
+});
+$(document).ready(function () {
+  let rotate = 0;
+  $('#summaryPudins').click(()=>{
+      rotate += 180;
+      $('#caret-down-pudins').css('transform',`rotate(${rotate}deg)`)
+      if(rotate === 180){
+        rotate =- 180;
+      }
+  })
 });
